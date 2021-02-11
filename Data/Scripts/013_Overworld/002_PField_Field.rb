ba6a23d977acc616d2d9bb18b505607459da0e44
phantombass <tmkeaton@ngu.edu>
2021-02-11 06:29:26 -0500

    .

--- Data/Scripts/013_Overworld/002_PField_Field.rb
+++ Data/Scripts/013_Overworld/002_PField_Field.rb
@@ -179,29 +179,6 @@ end
 #===============================================================================
 # Pokérus check
 Events.onMapUpdate += proc { |_sender,_e|
-  def pbDisplay(text)
-    @messageBox.text    = text
-    @messageBox.visible = true
-    @helpWindow.visible = false
-    pbPlayDecisionSE
-    loop do
-      Graphics.update
-      Input.update
-      pbUpdate
-      if @messageBox.busy?
-        if Input.trigger?(Input::C)
-          pbPlayDecisionSE if @messageBox.pausing?
-          @messageBox.resume
-        end
-      else
-        if Input.trigger?(Input::B) || Input.trigger?(Input::C)
-          break
-        end
-      end
-    end
-    @messageBox.visible = false
-    @helpWindow.visible = true
-  end
   next if !$Trainer
   if $game_switches[141] == true
     pbDisplay(_INTL("You are now ready to play past the demo! Please go to Mauselynx Alley to continue your journey!"))
