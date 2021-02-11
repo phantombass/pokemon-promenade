294ebf06998083b510e9c0bb9c0b35ecf187ae73
phantombass <tmkeaton@ngu.edu>
2021-02-11 06:48:51 -0500

    Post Demo

--- Data/Scripts/013_Overworld/002_PField_Field.rb
+++ Data/Scripts/013_Overworld/002_PField_Field.rb
@@ -180,10 +180,6 @@ end
 # Pokérus check
 Events.onMapUpdate += proc { |_sender,_e|
   next if !$Trainer
-  if $game_switches[141] == true
-    pbMessage(_INTL("You are now ready to play past the demo! Please go to Mauselynx Alley to continue your journey!"))
-    $game_switches[142] = true
-  end
   last = $PokemonGlobal.pokerusTime
   now = pbGetTimeNow
   if !last || last.year!=now.year || last.month!=now.month || last.day!=now.day
