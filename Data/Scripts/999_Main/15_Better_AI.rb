25bb7084e08a6263e25b82ce076a4a5dc344a1b6
phantombass <tmkeaton@ngu.edu>
2021-03-28 11:50:17 -0400

    Map Fixes

--- Data/Scripts/999_Main/15_Better_AI.rb
+++ Data/Scripts/999_Main/15_Better_AI.rb
@@ -331,6 +331,7 @@ class PokeBattle_AI
     # Pokémon can't do anything (must have been in battle for at least 5 rounds)
     if !@battle.pbCanChooseAnyMove?(idxBattler) &&
        battler.turnCount && battler.turnCount>=0
+
       shouldSwitch = true
     end
     # Pokémon is Perish Songed and has Baton Pass
@@ -718,7 +719,8 @@ class PokeBattle_AI
 			end
 			# Pick a good move for the Choice items
 			if user.hasActiveItem?([:CHOICEBAND,:CHOICESPECS,:CHOICESCARF])
-				if move.baseDamage>=60;     score += 60
+				if user.effects[PBEffects::ChoiceBand]>=0 && move.id == user.effects[PBEffects::ChoiceBand];  score += 100
+			  elsif move.baseDamage>=60;     score += 60
 				elsif move.damagingMove?;   score += 30
 				elsif move.function=="0F2"; score += 70   # Trick
 				else;                       score -= 60
