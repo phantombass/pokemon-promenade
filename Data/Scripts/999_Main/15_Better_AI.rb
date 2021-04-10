24dd9f4778e192add17b04ba3b9a5198c8dfac47
phantombass <tmkeaton@ngu.edu>
2021-04-10 14:13:07 -0400

    AI Fix

--- Data/Scripts/999_Main/15_Better_AI.rb
+++ Data/Scripts/999_Main/15_Better_AI.rb
@@ -718,7 +718,10 @@ class PokeBattle_AI
 			end
 			# Pick a good move for the Choice items
 			if user.hasActiveItem?([:CHOICEBAND,:CHOICESPECS,:CHOICESCARF])
-				if move.baseDamage>=60;     score += 60
+				if user.effects[PBEffects::ChoiceBand]>=0 && move.id == user.effects[PBEffects::ChoiceBand]
+					score += 100
+					shouldSwitch = false if @battle.pbSideSize(user.index)==1
+			  elsif move.baseDamage>=60;     score += 60
 				elsif move.damagingMove?;   score += 30
 				elsif move.function=="0F2"; score += 70   # Trick
 				else;                       score -= 60
