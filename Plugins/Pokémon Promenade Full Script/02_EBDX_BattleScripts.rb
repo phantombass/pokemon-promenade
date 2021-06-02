24ab7c8ce38abe300612bcf188cc4af035dc29d0
phantombass <tmkeaton@ngu.edu>
2021-06-01 23:15:34 -0400

    Added some BattleScripts Updated Zharonian Deino line's sprites

--- Plugins/Pokémon Promenade Full Script/02_EBDX_BattleScripts.rb
+++ Plugins/Pokémon Promenade Full Script/02_EBDX_BattleScripts.rb
@@ -26,6 +26,11 @@ def burnAllPokemon(event=nil)
      end
 end
 
+module EliteBattle
+  TRAINER_SPRITE_SCALE = 1
+  REPLACE_MISSING_ANIM = true
+end
+
 class PokeBattle_Battle
   def removeAllHazards
     if @battlers[0].pbOwnSide.effects[PBEffects::StealthRock] || @battlers[0].pbOpposingSide.effects[PBEffects::StealthRock]
@@ -49,6 +54,155 @@ class PokeBattle_Battle
       @battlers[0].pbOpposingSide.effects[PBEffects::StickyWeb] = false
     end
   end
+end
+
+EliteBattle.defineMoveAnimation(:STELLARWIND) do
+  vector = @scene.getRealVector(@targetIndex, @targetIsPlayer)
+  vector2 = @scene.getRealVector(@userIndex, @userIsPlayer)
+  # set up animation
+  fp = {}
+  rndx = []; prndx = []
+  rndy = []; prndy = []
+  rangl = []
+  dx = []
+  dy = []
+  for i in 0...128
+    fp["#{i}"] = Sprite.new(@viewport)
+    fp["#{i}"].bitmap = pbBitmap("Graphics/EBDX/Animations/Moves/eb423")
+    fp["#{i}"].ox = fp["#{i}"].bitmap.width/2
+    fp["#{i}"].oy = fp["#{i}"].bitmap.height/2
+    fp["#{i}"].visible = false
+    fp["#{i}"].z = @targetSprite.z + 1
+    rndx.push(rand(256)); prndx.push(rand(72))
+    rndy.push(rand(256)); prndy.push(rand(72))
+    rangl.push(rand(9))
+    dx.push(0)
+    dy.push(0)
+  end
+  shake = 4
+  # start animation
+  @vector.set(vector2)
+  pbSEPlay("Anim/Whirlwind")
+  for i in 0...72
+    ax, ay = @userSprite.getCenter
+    cx, cy = @targetSprite.getCenter(true)
+    for j in 0...128
+      next if j>(i*2)
+      if !fp["#{j}"].visible
+        dx[j] = ax - 46*@userSprite.zoom_x*0.5 + prndx[j]*@userSprite.zoom_x*0.5
+        dy[j] = ay - 46*@userSprite.zoom_y*0.5 + prndy[j]*@userSprite.zoom_y*0.5
+        fp["#{j}"].x = dx[j]
+        fp["#{j}"].y = dy[j]
+        fp["#{j}"].visible = true
+      end
+      x0 = ax - 46*@userSprite.zoom_x*0.5 + prndx[j]*@userSprite.zoom_x*0.5
+      y0 = ay - 46*@userSprite.zoom_y*0.5 + prndy[j]*@userSprite.zoom_y*0.5
+      x2 = cx - 128*@targetSprite.zoom_x*0.5 + rndx[j]*@targetSprite.zoom_x*0.5
+      y2 = cy - 128*@targetSprite.zoom_y*0.5 + rndy[j]*@targetSprite.zoom_y*0.5
+      fp["#{j}"].x += (x2 - x0)*0.1
+      fp["#{j}"].y += (y2 - y0)*0.1
+      fp["#{j}"].angle += rangl[j]*2
+      nextx = fp["#{j}"].x
+      nexty = fp["#{j}"].y
+      if !@targetIsPlayer
+        fp["#{j}"].opacity -= 51 if nextx > cx && nexty < cy
+      else
+        fp["#{j}"].opacity -= 51 if nextx < cx && nexty > cy
+      end
+    end
+    if i >= 64
+  #    @targetSprite.x += 64*(@targetIsPlayer ? -1 : 1)
+    elsif i >= 52
+      @targetSprite.ox += shake
+      shake = -4 if @targetSprite.ox > @targetSprite.bitmap.width/2 + 2
+      shake = 4 if @targetSprite.ox < @targetSprite.bitmap.width/2 - 2
+      @targetSprite.still
+    end
+    @vector.set(vector) if i == 16
+    @vector.inc = 0.1 if i == 16
+    @scene.wait(1,i < 64)
+  end
+#  @targetSprite.visible = false
+#  @targetSprite.hidden = true
+#  @targetSprite.ox = @targetSprite.bitmap.width/2
+  pbDisposeSpriteHash(fp)
+  @vector.reset
+  @vector.inc = 0.2
+  @scene.wait(16,true)
+end
+EliteBattle.defineMoveAnimation(:TIMEWIND) do
+  vector = @scene.getRealVector(@targetIndex, @targetIsPlayer)
+  vector2 = @scene.getRealVector(@userIndex, @userIsPlayer)
+  # set up animation
+  fp = {}
+  rndx = []; prndx = []
+  rndy = []; prndy = []
+  rangl = []
+  dx = []
+  dy = []
+  for i in 0...128
+    fp["#{i}"] = Sprite.new(@viewport)
+    fp["#{i}"].bitmap = pbBitmap("Graphics/EBDX/Animations/Moves/eb423")
+    fp["#{i}"].ox = fp["#{i}"].bitmap.width/2
+    fp["#{i}"].oy = fp["#{i}"].bitmap.height/2
+    fp["#{i}"].visible = false
+    fp["#{i}"].z = @targetSprite.z + 1
+    rndx.push(rand(256)); prndx.push(rand(72))
+    rndy.push(rand(256)); prndy.push(rand(72))
+    rangl.push(rand(9))
+    dx.push(0)
+    dy.push(0)
+  end
+  shake = 4
+  # start animation
+  @vector.set(vector2)
+  pbSEPlay("Anim/Whirlwind")
+  for i in 0...72
+    ax, ay = @userSprite.getCenter
+    cx, cy = @targetSprite.getCenter(true)
+    for j in 0...128
+      next if j>(i*2)
+      if !fp["#{j}"].visible
+        dx[j] = ax - 46*@userSprite.zoom_x*0.5 + prndx[j]*@userSprite.zoom_x*0.5
+        dy[j] = ay - 46*@userSprite.zoom_y*0.5 + prndy[j]*@userSprite.zoom_y*0.5
+        fp["#{j}"].x = dx[j]
+        fp["#{j}"].y = dy[j]
+        fp["#{j}"].visible = true
+      end
+      x0 = ax - 46*@userSprite.zoom_x*0.5 + prndx[j]*@userSprite.zoom_x*0.5
+      y0 = ay - 46*@userSprite.zoom_y*0.5 + prndy[j]*@userSprite.zoom_y*0.5
+      x2 = cx - 128*@targetSprite.zoom_x*0.5 + rndx[j]*@targetSprite.zoom_x*0.5
+      y2 = cy - 128*@targetSprite.zoom_y*0.5 + rndy[j]*@targetSprite.zoom_y*0.5
+      fp["#{j}"].x += (x2 - x0)*0.1
+      fp["#{j}"].y += (y2 - y0)*0.1
+      fp["#{j}"].angle += rangl[j]*2
+      nextx = fp["#{j}"].x
+      nexty = fp["#{j}"].y
+      if !@targetIsPlayer
+        fp["#{j}"].opacity -= 51 if nextx > cx && nexty < cy
+      else
+        fp["#{j}"].opacity -= 51 if nextx < cx && nexty > cy
+      end
+    end
+    if i >= 64
+  #    @targetSprite.x += 64*(@targetIsPlayer ? -1 : 1)
+    elsif i >= 52
+      @targetSprite.ox += shake
+      shake = -4 if @targetSprite.ox > @targetSprite.bitmap.width/2 + 2
+      shake = 4 if @targetSprite.ox < @targetSprite.bitmap.width/2 - 2
+      @targetSprite.still
+    end
+    @vector.set(vector) if i == 16
+    @vector.inc = 0.1 if i == 16
+    @scene.wait(1,i < 64)
+  end
+#  @targetSprite.visible = false
+#  @targetSprite.hidden = true
+#  @targetSprite.ox = @targetSprite.bitmap.width/2
+  pbDisposeSpriteHash(fp)
+  @vector.reset
+  @vector.inc = 0.2
+  @scene.wait(16,true)
 end
 
 module BattleScripts
@@ -91,7 +245,7 @@ module BattleScripts
       @battle.field.weather = :Sandstorm
       @battle.field.weatherDuration = 8
       @scene.pbDisplay("The sandstorm resurged!")
-      @scene.wait(8,false)
+      @scene.wait(16,false)
       if @battle.battlers[0].pbOwnSide.effects[PBEffects::StealthRock] == false
         @battle.pbAnimation(getID(GameData::Move,:STEALTHROCK),battle.battlers[1],battle.battlers[0])
         @battle.battlers[0].pbOwnSide.effects[PBEffects::StealthRock] = true
@@ -100,6 +254,22 @@ module BattleScripts
     end,
     "turnStart0" => "Let's see just what you're made of!"
     }
+
+RALPH = {
+    "lastOpp" => proc do
+              @scene.pbTrainerSpeak("May the sun rise on our victory!")
+              @sprites["battlebg"].reconfigure(:MAGMA, :DISTORTION)
+              @battle.field.defaultWeather = :HarshSun
+              @battle.field.weather = :HarshSun
+              @scene.pbDisplay("The sun returned and intensified!")
+              @scene.wait(16,false)
+              EliteBattle.playCommonAnimation(:BURN,@scene,0)
+              @battle.battlers[0].status = :BURN
+              burnAllPokemon(nil)
+              @scene.pbDisplay("The intense sun left \\PN's team burned!")
+            end,
+    "turnStart0" => "Let's get this place heated up!"
+            }
 
   RIVAL1 = { "turnStart0" => "I'm so stoked to see your Pokémon!",
              "lowHPOpp" => "Whoa, that little guy is strong!"
